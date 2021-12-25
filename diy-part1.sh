c5cc8e2f90296b1d8577417c858c0aecf3ff5d94
longlll <274257651@qq.com>
2021-12-26 03:23:16 +0800

    2
    2

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,18 +14,15 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '1i src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
 sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+sed -i '$a src-git ssrp https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git packages https://github.com/coolsnowwolf/packages' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
+sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
+sed -i '$a src-git ctr54188 https://github.com/ctr54188/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git NanoHatOLED https://github.com/ctr54188/NanoHatOLED.git' feeds.conf.default
 
-#Add external packages
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/others/luci-app-openclash
-#git submodule add https://github.com/destan19/OpenAppFilter.git package/others/OpenAppFilter
-#git submodule add https://github.com/tty228/luci-app-serverchan.git package/others/luci-app-serverchan
-#git submodule add -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/others/luci-theme-edge
-#git submodule add https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/others/luci-theme-infinityfreedom
-#git submodule add https://github.com/garypang13/luci-app-eqos.git package/others/luci-app-eqos
-#git submodule add https://github.com/lisaac/luci-app-dockerman.git package/others/luci-app-dockerman
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
 
